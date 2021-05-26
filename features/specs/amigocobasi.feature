5ee8b245ab4fb305670c35b13ac8037ccda3bb35
Gabriel Araujo Maitha <gabriel.lopes.ara@gmail.com>
2021-05-26 15:46:40 -0300

    Amigo cobasi

--- features/specs/amigocobasi.feature
+++ features/specs/amigocobasi.feature
@@ -7,3 +7,20 @@ Funcionalidade: Amigo cobasi
             | senha | Ga250400              |
             E entre no menu amigo cobasi
         Então tenho que ver o quadro de pontos
+    @cadastroamigocobasi
+    Cenario: Op-tin amigo Cobasi menu mais
+        Dado que eu realize o login com
+            | email | gabriel.lopes.ara@gmail.com |
+            | senha | Ga250400                    |
+            E selecione faça a desão gratuita aqui
+            E aceite os termos
+            # E clicar em fazer adesão
+        # Então tenho que ver o quadro de pontos
+    @trocadepontos
+    Cenario: Resgate de pontos
+        Dado que eu realize o login com
+            | email | ftamburus@gmail.com |
+            | senha | 123@Cobasi                    |
+            E entre no menu amigo cobasi
+            E seleciono um desconto para ser resgatado
+            E faço o swipe para ir para tela de confirmação dos pontos
--- features/steps/amigo_cobasi_steps.rb
+++ features/steps/amigo_cobasi_steps.rb
@@ -15,3 +15,31 @@ Então("tenho que ver o quadro de pontos") do
   elemento = find_element(xpath: "//android.widget.TextView[@text='Seus pontos']")
   expect(elemento.displayed?).to be true
 end
+
+Dado("selecione faça a desão gratuita aqui") do
+  find_element(xpath: "//android.widget.TextView[@text='Faça sua adesão gratuita aqui']").click
+end
+
+Dado("aceite os termos") do
+  #checbok está com bug
+  # find_element(xpath: "//android.widget.CheckBox").click
+  find_element(xpath: "//android.view.ViewGroup[@index='17']").click
+end
+
+Dado("clicar em fazer adesão") do
+  find_element(xpath: "//android.widget.CheckBox[@checked='true']")
+  find_element(xpath: "//android.widget.Button").click
+end
+
+Dado("seleciono um desconto para ser resgatado") do
+  find_element(xpath: "//android.widget.TextView[@text='Seus pontos']")
+  scrool = { start_x: 0.46, start_y: 0.49, offset_x: 0.46, offset_y: 0.80, duration: 2000 }
+  Appium::TouchAction.new.swipe(scrool).perform
+  descontos = find_elements(xpath: "//android.widget.CheckBox[@clickable='true']")
+  descontos[1].click
+end
+
+Dado("faço o swipe para ir para tela de confirmação dos pontos") do
+  swipe = { start_x: 276, start_y: 1826, end_x: 467, end_y: 1826, duration: 2000 }
+  Appium::TouchAction.new.swipe(swipe).perform
+end
